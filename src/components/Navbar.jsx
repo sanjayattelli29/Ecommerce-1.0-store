bacb1cdc1ea82c38193c7ef78693c485e6f87adc
rohitburugoju <rohitburugoju@gmail.com>
2025-02-18 21:22:23 +0530

    Initial commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,45 +1,37 @@
-import {FaShoppingCart} from "react-icons/fa"
+import { FaShoppingCart } from "react-icons/fa";
 import { useSelector } from "react-redux";
 import { NavLink } from "react-router-dom";
 
 const Navbar = () => {
-  const {cart} = useSelector((state) => state);
+  const { cart } = useSelector((state) => state);
 
   return (
     <div>
       <nav className="flex justify-between items-center h-20 max-w-6xl mx-auto">
-      <NavLink to="/">
-        <div className="ml-5"> 
-          <img src="logo.png" className="h-14"/>
-        </div>
-
-      </NavLink>
+        <NavLink to="/">
+          <div className="ml-5"> 
+            <img src="./logo.png" alt="Website Logo" className="h-12 w-auto" />
+          </div>
+        </NavLink>
 
         <div className="flex items-center font-medium text-slate-100 mr-5 space-x-6">
-          <NavLink to="/">
+          <NavLink to="/" className="hover:text-gray-300">
             <p>Home</p>
           </NavLink>
 
-
           <NavLink to="/cart">
             <div className="relative">
-              <FaShoppingCart className="text-2xl"/>
-              {
-                cart.length > 0 &&
-                <span 
-                className="absolute -top-1 -right-2 bg-green-600 text-xs w-5 h-5 flex justify-center items-center animate-bounce rounded-full text-white">
-                {cart.length}
+              <FaShoppingCart className="text-2xl" />
+              {cart.length > 0 && (
+                <span className="absolute -top-1 -right-2 bg-green-600 text-xs w-5 h-5 flex justify-center items-center animate-bounce rounded-full text-white">
+                  {cart.length}
                 </span>
-
-              }
+              )}
             </div>
           </NavLink>
-
-
         </div>
       </nav>
     </div>
-  
   );
 };
 
